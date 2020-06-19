deee1faafe7c0f6c843f7b1a9b674e6ef5ca4730
JK-97 <benjamin-dream@outlook.com>
2020-06-19 14:54:28 +0800

    waitprocess

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/JK-97/go-utils
+module github.com/JK-97/goutils
 
 go 1.13
 
-require go.uber.org/zap v1.14.0
+require (
+	go.uber.org/zap v1.14.0
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+)
---
+++ waitprocess.go
@@ -0,0 +1,79 @@
+package goutils
+
+import (
+	"context"
+	"reflect"
+	"runtime"
+	"time"
+
+	"github.com/JK-97/goutils/logger"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func CheckErr(err error) {
+	if err != nil {
+		logger.Error(err)
+	}
+}
+
+func RunAndLogError(f func() error) {
+	err := f()
+	if err != nil {
+		logger.Error(err)
+	}
+}
+
+func RunUntilSuccess(ctx context.Context, retryInterval time.Duration, f func() error) error {
+	err := f()
+	if err == nil {
+		return nil
+	}
+
+	ticker := time.NewTicker(retryInterval)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return err
+		case <-ticker.C:
+			err = f()
+			if err == nil {
+				return nil
+			}
+		}
+	}
+}
+
+const (
+	funcRestartWait = time.Second
+)
+
+// GoAndRestartOnError runs function in background associated with error group, until context done. Always restart if failed.
+func GoAndRestartOnError(ctx context.Context, errGroup *errgroup.Group, name string, f func() error) {
+	errGroup.Go(func() error { return RunAndRestartOnError(ctx, name, f) })
+}
+
+// RunAndRestartOnError runs function until context done. Always restart if failed.
+func RunAndRestartOnError(ctx context.Context, name string, f func() error) error {
+	for {
+		logger.Infof("starting %s", name)
+		err := f()
+		if err != nil {
+			logger.Errorf("%s stopped: %v", name, err)
+		}
+
+		select {
+		case <-ctx.Done():
+			return err
+		default:
+		}
+
+		logger.Infof("%s will restart in %v", name, funcRestartWait)
+		time.Sleep(funcRestartWait)
+	}
+}
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
